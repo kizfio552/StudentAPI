76579c1059b37b0cdc91c60a0103455750a62d9c
Kiz <Kiz@LAPTOP-HSCJSNH7>
2023-03-23 13:14:40 +0700

    Add project files.

---
+++ StudentAPI/Controllers/StudentController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentAPI.Models;
+using StudentAPI.Data;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public StudentController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        // Create/Edit
+        [HttpPost]
+        public JsonResult CreateEdit (Student student)
+        {
+            if (student.Id == 0)
+                _context.Students.Add(student);
+            else
+            {
+                var studentInDb = _context.Students.Find(student.Id);
+                if (studentInDb == null)
+                    return new JsonResult(NotFound());
+                studentInDb = student;
+            }
+            _context.SaveChanges();
+            return new JsonResult(Ok(student));
+        }
+
+        // Get
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Students.Find(id);
+            if (result == null)
+                return new JsonResult(NotFound());
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var ressult = _context.Students.Find(id);
+            if (ressult == null)
+                return new JsonResult(NotFound());
+            _context.Students.Remove(ressult);
+            _context.SaveChanges();
+            return new JsonResult(Ok(NoContent()));
+        }
+
+        // Get All
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var result = _context.Students.ToList();
+            return new JsonResult(Ok(result));
+        }
+    }
+}
---
+++ StudentAPI/Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAPI.Models;
+
+namespace StudentAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options)
+            :base (options)
+        {
+        
+        }
+    }
+}
